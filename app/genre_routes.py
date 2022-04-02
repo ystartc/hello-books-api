e70c9e840f196d4ea689226495fc5553c0d02f8a
Becca Elenzil <becca@adadevelopersacademy.org>
2022-04-02 07:24:29 -0700

    remove to_dict and nested routes from this branch

--- app/genre_routes.py
+++ app/genre_routes.py
@@ -6,19 +6,6 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 genres_bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
 
-def validate_genre(genre_id):
-    try:
-        genre_id = int(genre_id)
-    except:
-        abort(make_response({"message":f"genre {genre_id} invalid"}, 400))
-
-    genre = Genre.query.get(genre_id)
-
-    if not genre:
-        abort(make_response({"message":f"genre {genre_id} not found"}, 404))
-
-    return genre
-
 @genres_bp.route("", methods=["POST"])
 def create_genre():
     request_body = request.get_json()
@@ -43,30 +30,4 @@ def read_all_genres():
         )
     return jsonify(genres_response)
 
-@genres_bp.route("/<genre_id>/books", methods=["POST"])
-def create_book(genre_id):
 
-    genre = validate_genre(genre_id)
-
-    request_body = request.get_json()
-    new_book = Book(
-        title=request_body["title"],
-        description=request_body["description"],
-        author_id=request_body["author_id"],
-        genres=[genre]
-    )
-    db.session.add(new_book)
-    db.session.commit()
-    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
-
-@genres_bp.route("/<genre_id>/books", methods=["GET"])
-def read_all_books(genre_id):
-    
-    genre = validate_genre(genre_id)
-
-    books_response = []
-    for book in genre.books:
-        books_response.append(
-            book.to_dict()
-        )
-    return jsonify(books_response)
