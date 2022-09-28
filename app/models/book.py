37989c90536c9abc453835e8325d83a52085e4f9
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-09-28 16:08:13 -0700

    Refactor code in routes that creates a dictionary from a Book model to a helper function in the Book class.

--- app/models/book.py
+++ app/models/book.py
@@ -4,3 +4,11 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
--- app/routes.py
+++ app/routes.py
@@ -41,23 +41,13 @@ def read_all_books():
 
     books_response = []
     for book in books:
-        books_response.append(
-            {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
-        )
+        books_response.append(book.to_dict())
     return jsonify(books_response)
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
     book = validate_book(book_id)
-    return {
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        }
+    return book.to_dict()
 
 @books_bp.route("/<book_id>", methods=["PUT"])
 def update_book(book_id):
