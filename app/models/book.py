8aea7aab7578e786075109fd7e690d1470da8c8e
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-10-13 12:33:13 -0700

    Remove attributes for next lesson

--- app/models/book.py
+++ app/models/book.py
@@ -4,8 +4,6 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
-    author = db.relationship("Author", back_populates="books")
 
     def to_dict(self):
         book_as_dict = {}
