5a21d34ab9de6fab0e1a43a087b390f831840cf8
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-10-13 12:14:06 -0700

    Add content from Refactoring topic

--- app/models/book.py
+++ app/models/book.py
@@ -4,3 +4,17 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_dict(self):
+        book_as_dict = {}
+        book_as_dict["id"] = self.id
+        book_as_dict["title"] = self.title
+        book_as_dict["description"] = self.description
+
+        return book_as_dict
+
+    @classmethod
+    def from_dict(cls, book_data):
+        new_book = Book(title=book_data["title"],
+                        description=book_data["description"])
+        return new_book
