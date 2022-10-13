e5efebfcb33a7c374c28dcfd304601a34d584e74
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-10-13 12:29:48 -0700

    Add back changed Book attributes

--- app/models/book.py
+++ app/models/book.py
@@ -4,6 +4,8 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    author = db.relationship("Author", back_populates="books")
 
     def to_dict(self):
         book_as_dict = {}
