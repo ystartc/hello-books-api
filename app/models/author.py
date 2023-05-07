65d38bd7aeae74ef10258310baa6f2ae56f1f781
Yulia S <startyupp@gmail.com>
2023-05-06 21:54:33 -0700

    created to_dict instance method

--- app/models/author.py
+++ app/models/author.py
@@ -1,6 +1,13 @@
 from app import db
 
 class Author(db.Model):
+    __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
+    
+    def to_dict(self):
+        return {
+                'id': self.id,
+                'name': self.name
+                }
     
