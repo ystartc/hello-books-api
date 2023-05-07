94bd739278f33bac4e6c4fcc6ce6495d5a846340
Yulia S <startyupp@gmail.com>
2023-05-06 23:00:21 -0700

    established one-to-many relationship

--- app/models/author.py
+++ app/models/author.py
@@ -4,6 +4,7 @@ class Author(db.Model):
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50))
+    books = db.relationship('Book', back_populates='author')
     
     def to_dict(self):
         return {
--- app/models/book.py
+++ app/models/book.py
@@ -5,6 +5,8 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.Foreignkey('author.id'))
+    author = db.relationship('Author', back_populates='books')
     
     def to_string(self):
         return f'{self.id}: {self.title}. Description: {self.description}.'
