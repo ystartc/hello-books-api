990ce2a2fa1c95beb9196ccd0125c4dc94a0a365
Yulia S <startyupp@gmail.com>
2023-05-10 23:32:40 -0700

    changed author_id to be null if not defined

--- app/models/book.py
+++ app/models/book.py
@@ -5,7 +5,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=True)
     author = db.relationship('Author', back_populates='books')
     
     def to_string(self):
